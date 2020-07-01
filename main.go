8ec84c62e3737473016959492594d4aa849db7a2
gut <yuta1402souja@live.jp>
2020-07-01 13:25:08 +0900

    Refactoring

--- main.go
+++ main.go
@@ -11,6 +11,25 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot/httphandler"
 )
 
+type WordCommand struct {
+	AllHookWords []string
+	MakeReply    func(string) linebot.SendingMessage
+}
+
+func (wc WordCommand) canHook(text string) bool {
+	for _, word := range wc.AllHookWords {
+		if !strings.Contains(text, word) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func replyPoohChan(string) linebot.SendingMessage {
+	return linebot.NewTextMessage("ぷぅちゃん！")
+}
+
 func main() {
 	handler, err := httphandler.New(
 		os.Getenv("CHANNEL_SECRET"),
@@ -27,6 +46,11 @@ func main() {
 		log.Printf("Defaulting to port %s", port)
 	}
 
+	commands := []WordCommand{
+		{[]string{"ぷぅちゃん"}, replyPoohChan},
+		{[]string{"ぷーちゃん"}, replyPoohChan},
+	}
+
 	handler.HandleEvents(func(events []*linebot.Event, r *http.Request) {
 		bot, err := handler.NewClient()
 		if err != nil {
@@ -41,11 +65,12 @@ func main() {
 
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if strings.Contains(message.Text, "ぷぅちゃん") {
-					replyText := "ぷぅちゃん！"
-
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyText)).Do(); err != nil {
-						log.Print(err)
+				for _, command := range commands {
+					if command.canHook(message.Text) {
+						replyMessage := command.MakeReply(message.Text)
+						if _, err = bot.ReplyMessage(event.ReplyToken, replyMessage).Do(); err != nil {
+							log.Print(err)
+						}
 					}
 				}
 			}
